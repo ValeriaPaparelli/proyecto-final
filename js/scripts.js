aaf2ef8e13dc8cfc1123cb2c28af1c0a4350a66a
Valeria Paparelli <valeria.paparelli@live.com.ar>
2021-11-12 20:04:56 -0300

    agregando el html del carrito a todas las páginas

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+{/* <li class="header__menu__carrito__detalle__productos__item">
+    <span class="header__menu__carrito__detalle__productos__nombre">
+        Crema de manos
+    </span>
+    <span class="header__menu__carrito__detalle__productos__canidad">
+        x2
+    </span>
+    <span class="header__menu__carrito__detalle__productos__subtotal">
+        $1230,80
+    </span>
+</li> */}
+{/* <li class="header__menu__carrito__detalle__productos__vacio">
+    El Carrito está vacío
+</li>  */}
