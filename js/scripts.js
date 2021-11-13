b0497067528d6a1a25e7110d5efb0f36e2b47cc5
Valeria Paparelli <valeria.paparelli@live.com.ar>
2021-11-13 13:03:44 -0300

    agregando total y botones para eliminar productos del carrito

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,57 @@ $(() => {
     let mostrarCarrito = false;
 
     const agregarEventoClickProductos = () => {
-        productos.forEach((producto, i) => {
-            $(`#producto-${i}`).on('click', () => {
-                agregarProducto(producto);
+        if(typeof productos !== 'undefined') {
+            productos.forEach((producto, i) => {
+                $(`#producto-${i}`).on('click', () => {
+                    agregarProducto(producto);
+                });
+            });
+        }
+    };
+
+    const agregarEventoClickCarrito = () => {
+        miCarrito.items.forEach((producto, i) => {
+            $(`#producto-carrito-${i}`).on('click', () => {
+                eliminarProducto(producto);
             });
         });
     };
     
     const actualizarCarritoDOM = () => {
         $('#carrito-cantidad').html(`${miCarrito.items.length}`);
-        $('#total').html(`$${miCarrito.total}`);
+        $('#carrito-total').html(`$${miCarrito.total}`);
         $('#carrito-productos').html('');
 
         if (miCarrito.items.length === 0) {
+            $('#carrito-total-contenedor').hide();
+            $('#carrito-botones').hide();
             $('#carrito-productos').html(`<li class="header__menu__carrito__detalle__productos__vacio">
                 El Carrito está vacío
             </li>`);
             return;
         }
-    
+        
+        $('#carrito-total-contenedor').show();
+        $('#carrito-botones').show();
+
         miCarrito.items.forEach((producto, i) => {
-            const detalleCarrito = `
+            const productosCarrito = `
                 <li class="header__menu__carrito__detalle__productos__item">
                     <span class="header__menu__carrito__detalle__productos__nombre">
                         ${producto.nombre}
                     </span>
-                    <span class="header__menu__carrito__detalle__productos__canidad">
-                        x2
-                    </span>
                     <span class="header__menu__carrito__detalle__productos__subtotal">
-                        $${producto.precio} x
+                        $${producto.precio}
+                    </span>
+                    <span class="header__menu__carrito__detalle__productos__eliminar">
+                        <button id="producto-carrito-${i}">X</button>
                     </span>
                 </li>`;
-            $('#carrito-productos').append(detalleCarrito);
+            $('#carrito-productos').append(productosCarrito);
         });
+
+        agregarEventoClickCarrito();
     };
     
     const agregarProducto = (producto) => {
@@ -64,6 +81,7 @@ $(() => {
     const miCarrito = new Carrito(carritoInicial);
 
     actualizarCarritoDOM();
+    agregarEventoClickProductos();
 
     $('#carrito').on('click', () => {
         if (mostrarCarrito) {
@@ -81,20 +99,4 @@ $(() => {
 
     $('#carrito-vaciar').on('click', () => eliminarProductos());
 
-    agregarEventoClickProductos();
 });
-
-{/* <li class="header__menu__carrito__detalle__productos__item">
-    <span class="header__menu__carrito__detalle__productos__nombre">
-        Crema de manos
-    </span>
-    <span class="header__menu__carrito__detalle__productos__canidad">
-        x2
-    </span>
-    <span class="header__menu__carrito__detalle__productos__subtotal">
-        $1230,80
-    </span>
-</li> */}
-{/* <li class="header__menu__carrito__detalle__productos__vacio">
-    El Carrito está vacío
-</li>  */}
